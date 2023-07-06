af5f66152723b673fa2ffe709727ad0e2aa1f6fe
tomasgpou <tomas.gomezpou@gmail.com>
2023-07-06 09:57:53 -0300

    Se incorpora clase, array y funciones al simulador de compra

--- JS/main.js
+++ JS/main.js
@@ -1,35 +1,61 @@
 // productos
-const prod1 = 3000
-const prod2 = 4500
-const prod3 = 5500
+class Product{
+    constructor(id,name,category,price,stock){
+        this.id       = this.id
+        this.name     = name.toUpperCase()
+        this.category = category.toUpperCase()
+        this.price    = parseFloat(price)
+        this.stock    = parseInt(stock)
+        this.isStock  = true
+    }
+    sinStock(){
+        this.isStock  = false
+    }
+}
 
-// seleccionar de producto
-let producto = prompt('Ingrese nro de producto (1, 2 o 3):')
+const products = []
 
-switch (producto) {
-    case '1':
-        prod = prod1
-        break;
-    case '2':
-        prod = prod2
-        break;
-    case '3':
-        prod = prod3
+products.push (new Product (1,'Remera Negra','remeras',5500,20))
+products.push (new Product (2,'Remera Naranja','remeras',5500,20))
+products.push (new Product (3,'Remera Blanca','remeras',4500,10))
+products.push (new Product (4,'gorra 1','gorras',3000,10))
+
+console.table(products);
+
+function forEach(arr,cb) {
+    for (const element of arr) {
+        cb(element)
+    }
+}
+
+// Simulador de compra
+let amount = 0
+let cart = 0
+
+function addToCart(element) {
+    amount = parseInt(prompt(`Producto: ${element.name}. Ingrese la cantidad que desea:`))
+
+    while (amount < 0 || isNaN(amount)) {
+        amount = parseInt(prompt("Ingrese un valor correcto:"));
+    }
+    cart += (element.price * amount)
+}
+
+let option = prompt('Deseas filtrar productos por categoria? (SI/NO)').toUpperCase()
+switch (option) {
+    case 'SI':
+        let selectedCategory = prompt('Ingrese categoria (Remeras/Gorras):').toUpperCase()
+        let selectedProducts = products.filter((productsObj) => productsObj.category === selectedCategory)
+        console.table(selectedProducts);
+
+        forEach (selectedProducts,addToCart)
+        alert('El valor total de los productos es: $' + cart);
         break;
 
+    case 'NO':
+        forEach (products,addToCart)
+        alert('El valor total de los productos es: $' + cart);
+        break;
     default:
-        producto = prompt('Ingrese un nro de producto correcto (1, 2 o 3)')
         break;
 }
-
-// seleccionar cantidad
-let cantidad = parseInt(prompt('Ingrese la cantidad que desea:'))
-
-// validacion cantidad
-while ((cantidad < 1) || (isNaN(cantidad))) {
-    cantidad = parseInt(prompt('Ingrese un valor correcto:'))
-}
-
-// calculadora de valor total
-const valorTotal = (prod, cantidad) => prod * cantidad
-alert('El valor total de los productos es: $'+ valorTotal(prod,cantidad));
